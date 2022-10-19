b78f55969b70b033a1a19df2327e32b27de60632
sameer <sameersahu200138@gmail.com>
2022-10-19 23:49:14 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import styles from './app.css'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline bg-slate-700">
+    Hello world!
+  </h1>
+  );
+}
+
+export default App;
